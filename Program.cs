3ec2abc24e90066e8ecf996887edb57a6d1b3ec2
Diego <cortesroadiegoalejandro@gmail.com>
2023-03-14 14:42:48 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace qa_automation_test_teaminternational
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello, World!");
+    }
+  }
+}
